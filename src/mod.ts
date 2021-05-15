0ceb0ccda4f6cbccebc5c0b01c6d11a30086dda6
fourside <fourside@gmail.com>
2021-05-15 09:57:47 +0900

    run only if mod is entry point

--- src/mod.ts
+++ src/mod.ts
@@ -2,38 +2,46 @@ import { YoutubeDl } from "./youtube_dl.ts";
 import { chooseBestCodes, parse } from "./youtube_format.ts";
 import { parseArgs } from "./cli.ts";
 
-const result = parseArgs(Deno.args);
-if (result.exit) {
-  Deno.exit(result.exitCode);
+export async function main(args: string[]) {
+  const result = parseArgs(args);
+  if (result.exit) {
+    Deno.exit(result.exitCode);
+  }
+
+  try {
+    const youtubeDl = new YoutubeDl(result.url);
+    const detect = await youtubeDl.detectPrerequisite();
+    if (!detect.success) {
+      console.log(detect.message);
+      Deno.exit(-1);
+    }
+    const listFormat = await youtubeDl.listFormat();
+    console.info(listFormat);
+    const youtubeFormats = listFormat.map((format) => parse(format));
+
+    const videoFormats = youtubeFormats.filter((format) => format.isVideo());
+    if (videoFormats.length === 0) {
+      console.log("[ytdl-best] no video format");
+      Deno.exit(-1);
+    }
+    const audioFormats = youtubeFormats.filter((format) => format.isAudio());
+    if (audioFormats.length === 0) {
+      console.log("[ytdl-best] no audio format");
+      Deno.exit(-1);
+    }
+    const [videoCode, audioCode] = chooseBestCodes(videoFormats, audioFormats);
+    console.log(
+      `[ytdl-best] best video: ${videoCode}, best audio: ${audioCode}`,
+    );
+
+    await youtubeDl.download(videoCode, audioCode);
+    console.log("[ytdl-best] Done! :tada:");
+  } catch (error) {
+    console.error("[ytdl-best]", error);
+    Deno.exit(-1);
+  }
 }
 
-try {
-  const youtubeDl = new YoutubeDl(result.url);
-  const detect = await youtubeDl.detectPrerequisite();
-  if (!detect.success) {
-    console.log(detect.message);
-    Deno.exit(-1);
-  }
-  const listFormat = await youtubeDl.listFormat();
-  console.info(listFormat);
-  const youtubeFormats = listFormat.map((format) => parse(format));
-
-  const videoFormats = youtubeFormats.filter((format) => format.isVideo());
-  if (videoFormats.length === 0) {
-    console.log("[ytdl-best] no video format");
-    Deno.exit(-1);
-  }
-  const audioFormats = youtubeFormats.filter((format) => format.isAudio());
-  if (audioFormats.length === 0) {
-    console.log("[ytdl-best] no audio format");
-    Deno.exit(-1);
-  }
-  const [videoCode, audioCode] = chooseBestCodes(videoFormats, audioFormats);
-  console.log(`[ytdl-best] best video: ${videoCode}, best audio: ${audioCode}`);
-
-  await youtubeDl.download(videoCode, audioCode);
-  console.log("[ytdl-best] Done! :tada:");
-} catch (error) {
-  console.error("[ytdl-best]", error);
-  Deno.exit(-1);
+if (import.meta.main) {
+  await main(Deno.args);
 }
