170d305db2184dc592e8e19763fd0e660b34afcc
fourside <fourside@gmail.com>
2022-09-07 10:16:00 +0900

    support the latest list-formats

--- src/youtube_format.test.ts
+++ src/youtube_format.test.ts
@@ -4,7 +4,7 @@ import { chooseBestId, parse, YoutubeFormat } from "./youtube_format.ts";
 Deno.test("parse audio line webm", () => {
   // arrange
   const line =
-    "249 webm                 |    4.38MiB    53k https | audio only         opus        53k 48000Hz low, webm_dash";
+    "249 webm audio only      2 │  754.19KiB    50k https │ audio only         opus        50k 48k low, webm_dash";
   // act
   const result = parse(line);
 
@@ -12,63 +12,63 @@ Deno.test("parse audio line webm", () => {
   assert(result.type === "audio");
   assertEquals(result.id, "249");
   assertEquals(result.extension, "webm");
-  assertEquals(result.fileSize, "4.38MiB");
-  assertEquals(result.tbr, "53k");
+  assertEquals(result.fileSize, "754.19KiB");
+  assertEquals(result.tbr, "50k");
   assertEquals(result.proto, "https");
   assertEquals(result.audioCodec, "opus");
-  assertEquals(result.abr, "53k");
-  assertEquals(result.asr, "48000Hz");
+  assertEquals(result.abr, "50k");
+  assertEquals(result.asr, "48k");
   assertEquals(result.moreInfo, "low, webm_dash");
 });
 
 Deno.test("parse audio line m4a", () => {
   // arrange
   const line =
-    "140 m4a   audio only     |   10.63MiB   129k https | audio only         mp4a.40.2  129k 44100Hz medium, m4a_dash";
+    "139 m4a   audio only      2 │  722.92KiB   49k https │ audio only          mp4a.40.5   49k 22k low, m4a_dash";
   // act
   const result = parse(line);
 
   // assert
   assert(result.type === "audio");
-  assertEquals(result.id, "140");
+  assertEquals(result.id, "139");
   assertEquals(result.extension, "m4a");
-  assertEquals(result.fileSize, "10.63MiB");
-  assertEquals(result.tbr, "129k");
+  assertEquals(result.fileSize, "722.92KiB");
+  assertEquals(result.tbr, "49k");
   assertEquals(result.proto, "https");
-  assertEquals(result.audioCodec, "mp4a.40.2");
-  assertEquals(result.abr, "129k");
-  assertEquals(result.asr, "44100Hz");
-  assertEquals(result.moreInfo, "medium, m4a_dash");
+  assertEquals(result.audioCodec, "mp4a.40.5");
+  assertEquals(result.abr, "49k");
+  assertEquals(result.asr, "22k");
+  assertEquals(result.moreInfo, "low, m4a_dash");
 });
 
 Deno.test("parse video line webm, video only", () => {
   // arrange
   const line =
-    "244 webm  854x480     25 |   36.05MiB   439k https | vp9           439k video only              480p, webm_dash";
+    "278 webm  256x144     30    │    1.26MiB   87k https │ vp9             87k video only          144p, webm_dash";
   // act
   const result = parse(line);
 
   // assert
   assert(result.type === "video");
-  assertEquals(result.id, "244");
+  assertEquals(result.id, "278");
   assertEquals(result.extension, "webm");
-  assertEquals(result.resolution, "854x480");
-  assertEquals(result.fps, 25);
-  assertEquals(result.fileSize, "36.05MiB");
-  assertEquals(result.tbr, "439k");
+  assertEquals(result.resolution, "256x144");
+  assertEquals(result.fps, 30);
+  assertEquals(result.fileSize, "1.26MiB");
+  assertEquals(result.tbr, "87k");
   assertEquals(result.proto, "https");
   assertEquals(result.videoCodec, "vp9");
-  assertEquals(result.vbr, "439k");
+  assertEquals(result.vbr, "87k");
   assertEquals(result.audioCodec, "video only");
   assertEquals(result.abr, undefined);
   assertEquals(result.asr, undefined);
-  assertEquals(result.moreInfo, "480p, webm_dash");
+  assertEquals(result.moreInfo, "144p, webm_dash");
 });
 
 Deno.test("parse video line mp4, video only", () => {
   // arrange
   const line =
-    "135 mp4   854x480     25 |   27.15MiB   330k https | avc1.4d401e   330k video only              480p, mp4_dash";
+    "135 mp4   854x480     30    │    2.23MiB  155k https │ avc1.4d401f    155k video only          480p, mp4_dash";
   // act
   const result = parse(line);
 
@@ -77,12 +77,12 @@ Deno.test("parse video line mp4, video only", () => {
   assertEquals(result.id, "135");
   assertEquals(result.extension, "mp4");
   assertEquals(result.resolution, "854x480");
-  assertEquals(result.fps, 25);
-  assertEquals(result.fileSize, "27.15MiB");
-  assertEquals(result.tbr, "330k");
+  assertEquals(result.fps, 30);
+  assertEquals(result.fileSize, "2.23MiB");
+  assertEquals(result.tbr, "155k");
   assertEquals(result.proto, "https");
-  assertEquals(result.videoCodec, "avc1.4d401e");
-  assertEquals(result.vbr, "330k");
+  assertEquals(result.videoCodec, "avc1.4d401f");
+  assertEquals(result.vbr, "155k");
   assertEquals(result.audioCodec, "video only");
   assertEquals(result.abr, undefined);
   assertEquals(result.asr, undefined);
@@ -92,7 +92,7 @@ Deno.test("parse video line mp4, video only", () => {
 Deno.test("parse video line, not video only", () => {
   // arrange
   const line =
-    "22  mp4   1280x720    25 | ~ 59.45MiB   706k https | avc1.64001F   706k mp4a.40.2    0k 44100Hz 720p";
+    "22  mp4   1280x720    30  2 │ ~ 10.99MiB  744k https │ avc1.64001F    744k mp4a.40.2    0k 44k 720p";
   // act
   const result = parse(line);
 
@@ -101,22 +101,22 @@ Deno.test("parse video line, not video only", () => {
   assertEquals(result.id, "22");
   assertEquals(result.extension, "mp4");
   assertEquals(result.resolution, "1280x720");
-  assertEquals(result.fps, 25);
-  assertEquals(result.fileSize, "59.45MiB");
-  assertEquals(result.tbr, "706k");
+  assertEquals(result.fps, 30);
+  assertEquals(result.fileSize, "10.99MiB");
+  assertEquals(result.tbr, "744k");
   assertEquals(result.proto, "https");
   assertEquals(result.videoCodec, "avc1.64001F");
-  assertEquals(result.vbr, "706k");
+  assertEquals(result.vbr, "744k");
   assertEquals(result.audioCodec, "mp4a.40.2");
   assertEquals(result.abr, "0k");
-  assertEquals(result.asr, "44100Hz");
+  assertEquals(result.asr, "44k");
   assertEquals(result.moreInfo, "720p");
 });
 
 Deno.test("parse image line", () => {
   // arrange
   const line =
-    "sb0 mhtml 160x90         |                   mhtml | images                                     storyboard";
+    "sb0 mhtml 160x90       1    │                  mhtml │ images                                  storyboard";
   // act
   const result = parse(line);
 
